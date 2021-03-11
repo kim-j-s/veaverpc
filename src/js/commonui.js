96390fb8749632f665274a9d61ac62920c4387ae
kogwing <kogwing@gmail.com>
2021-03-11 11:22:56 +0900

    chat

--- src/js/commonui.js
+++ src/js/commonui.js
@@ -1,5 +1,167 @@
 $(function(){
 
+	//var elm = $('.content');
+    $('.content').each(function(index){
+        $(this).on('mousewheel DOMMouseScroll', function(e){
+            e.preventDefault();
+            var delta = 0;
+            var contLng = $('.content').length;
+
+            $('.nav-list').find('a').blur();
+
+            //console.log('animaion 1 : ' + animaion);
+            //console.log('index : ' + index);
+
+            if (animaion == false) {
+                animaion = true;
+            
+                // firefox를 제외한 브라우져 마우스 이벤트 값
+                if (event.wheelDelta) {
+                    delta = event.wheelDelta / 120;
+                    if (window.opera) delta = -delta;
+                } 
+                // firefox 브라우져 마우스 이벤트 값
+                else if (event.detail)
+                    delta = -event.detail / 3;
+
+                // up
+                if (delta > 0) {
+                    if ( index > 0) {
+                        console.log('상단으로 이동');
+                        wheelAnimate(index - 2);
+                    } else {
+                        console.log('up 최상단 도달 시');
+                        animaion = false;
+                    }
+                } 
+                // down
+                else if (delta < 0) {
+                    if ( (index + 1) < contLng) {
+                        console.log('down 1 내려간다 : ' + (index + 1), contLng);
+                        wheelAnimate(index);
+                    } else {
+                        console.log('down 2 : ' + (index + 1));
+                        animaion = false;
+                    }
+                }
+
+            }
+        })
+    })
+
+    // nav 이벤트
+    navEvent();
+
 
 	//ready
 });
+
+
+
+var animaion = false;
+
+
+// nav 이벤트
+function navEvent() {
+    console.log('xcvxcvxc');
+    var $navList = $('.nav-list').find('a');
+    $navList.on('click', function(e){
+        e.preventDefault();
+        var idx = $(this).closest('li').index();
+        console.log(idx);
+        wheelAnimate(idx);
+    })
+}
+
+// 페이지 휠 이동
+function wheelAnimate(index){
+    //console.log('iiiiiiii : ' + index);
+    var wh = $(window).height();
+    $('html, body').stop().animate({
+        scrollTop: (index + 1) * wh + 'px',
+    }, 500, function(){
+        animaion = false;
+        console.log('index : ' + index);
+    })
+
+    if (index < 0) {
+        $('.nav-list').find('a').removeClass('on');
+    } else {
+        $('.nav-list').find('a').removeClass('on');
+        $('.nav-list').find('a').eq(index).addClass('on');
+    }
+
+    if ( $('.content').eq(index).hasClass('chat-in') ) {
+		ChatList();
+    }
+}
+
+
+
+//ChatList();
+
+
+// chat list
+var chatMessages = [
+    {
+        name: "msg1",
+        msg: "나의 건강 기록을 한눈에 볼 수 있을까?",
+        delay: 10,
+        align: "chat-left",
+    },
+    {
+        name: "msg2",
+        msg: "건강365에서 건강보험공단에 등록된<br> 나의 건강 Data를 정리해 드립니다.",
+        delay: 1500,
+        align: "chat-right",
+    },
+    {
+        name: "msg3",
+        msg: "매일매일 스스로 건강관리는 어떻게 하나요?",
+        delay: 1500,
+        align: "chat-left",
+    },
+    {
+        name: "msg4",
+        msg: "주기적인 관리가 필요한 건강 데이터를<br> 건강365에서 기록하며 관리하세요!",
+        delay: 1500,
+        align: "chat-right",
+    }
+];
+
+function ChatList() {
+    var chatDelay = 0;
+    $.each(chatMessages, function(index, obj) {
+        chatDelay = chatDelay + 1000;
+        chatDelay2 = chatDelay + obj.delay;
+        chatDelay3 = chatDelay2 + 10;
+        chatTimeString = " ";
+        msgname = "." + obj.name;
+        spinner = ".sp-" + obj.name;
+
+        $(".chat-list").append(
+            "<li class=" + obj.align + ">" +
+                "<span class='txt'>" + obj.msg + "</span>" +
+                "<span class='sp-" + obj.name + "'>" +
+                    "<span class='spin-ele'>" +
+                        "<span class='spinner'>" + 
+                            "<span class='bounce1'></span>" +
+                            "<span class='bounce2'></span>" +
+                            "<span class='bounce3'></span>" +
+                        "</span>" + 
+                    "</span>" +
+                "</span>" +
+            "</li>"
+        );
+
+        // display      
+        $('.chat-list > li').eq(index).delay(chatDelay).animate({
+            opacity: 1,
+        }, 500);
+        $(spinner).delay(chatDelay2).hide(1);
+        $('.chat-list > li').eq(index).children('.txt').delay(chatDelay3).animate({
+            opacity: 1,
+        }, 500);
+        chatDelay = chatDelay3;
+    });
+}
